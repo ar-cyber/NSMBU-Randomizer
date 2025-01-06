0c138f4ea052032b07ebfc70426a163d92ff1d7a
RobinAnd Robinson <robin>
2025-01-06 11:45:12 +1030

    inital commit

---
+++ SortingFile.bat
@@ -0,0 +1,38 @@
+@echo off
+setlocal EnableDelayedExpansion
+cd %~dp0CopyOfFiles
+
+::generating a 'random number' that I wont use because this program is annoying 
+set m=%random%
+
+set loop=0
+:loop
+set /a loop=%loop%+1 
+if "%loop%"=="1000" goto end
+
+::calculating 2 random boi
+set n=0
+for %%f in (*.*) do (
+   set /A n+=1
+   set "file[!n!]=%%f"
+)
+::Generating an actual random number
+set /A "rand1=(n*%random%)/32768+1"
+set /A "rand2=(n*%random%)/32768+1"
+
+if "%rand1%" == "%rand2%" goto next
+echo %rand1%
+echo %rand2%
+::chose file from the first folder
+move "%~dp0CopyOfFiles\!file[%rand1%]!" "%~dp0CopyOfFiles\ThInGy"
+move "%~dp0CopyOfFiles\!file[%rand2%]!" "%~dp0CopyOfFiles\!file[%rand1%]!"
+move "%~dp0CopyOfFiles\ThInGy" "%~dp0CopyOfFiles\!file[%rand2%]!"
+rem pause
+:next
+
+goto loop
+
+:next
+
+:end
+exit
