774bd2a931880d30c38fbc63925cec77cef797f3
Andrew R <25roband@cbc.sa.edu.au>
2025-01-06 12:18:49 +1030

    Update _main.py

--- _main.py
+++ _main.py
@@ -1,24 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# SARC Tool
-# Version v0.5
-# Copyright © 2017-2019 MasterVermilli0n / AboodXD
+# NSMBU RANDOMIZER
+# This was a modified version of the SarcTool by AboodXD, running NinTyler's randomizer to jumble the files, then modifying the file in the level to not crash.
 
-# This is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
 
-# This is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-################################################################
 ################################################################
 
 import os
